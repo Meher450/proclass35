9f495a08c82bacae8bf3f21c988d1c2f6169aa9e
Meher450 <meherraju2006@gmail.com>
2021-06-03 18:40:55 +0530

    class35

--- sketch.js
+++ sketch.js
@@ -1,29 +1,45 @@
 var ball;
+ var hball,database,position;
+ 
+ function setup(){
+     database=firebase.database();
+     console.log(database)
 
-function setup(){
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
-}
-
-function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-    drawSprites();
-}
-
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
-}
+    hball = createSprite(250,250,10,10); 
+    hball.shapeColor = "red"; 
+    var hballposition=database.ref('ball/position')
+    hballposition.on("value",readPosition,showError)
+    } 
+    
+    function draw(){ 
+        background("white"); 
+        if(position!=undefined) { 
+            if(keyDown(LEFT_ARROW)){ 
+            changePosition(-1,0); 
+            } 
+            else if(keyDown(RIGHT_ARROW)){ 
+            changePosition(1,0); 
+            } 
+            else if(keyDown(UP_ARROW)){ 
+            changePosition(0,-1); 
+            } 
+            else if(keyDown(DOWN_ARROW)){ 
+            changePosition(0,+1); 
+            } 
+    drawSprites(); 
+        } 
+    } 
+    
+    function changePosition(x,y){ 
+        database.ref('ball/position').set({"x":position.x+x,"y":position.y+y})
+ } 
+    
+ function readPosition(data) { 
+    position=data.val()
+    hball.x=position.x;
+    hball.y=position.y;
+    } 
+    
+function showError(){ 
+    console.log("Error in writing to the database"); }
